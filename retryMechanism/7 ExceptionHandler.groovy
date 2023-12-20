cdfd163bc1587003a9ac4457a11bbbee271adc2e
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2023-12-20 10:50:31 -0300

    feat: retry mechanism

---
+++ retryMechanism/7 ExceptionHandler.groovy
@@ -0,0 +1,21 @@
+import com.sap.gateway.ip.core.customdev.util.Message
+
+def Message processData(Message message) {
+    def inboundPayload          = message.getBody(String)
+    def properties              = message.getProperties()
+    def camelExceptionCaught    = properties.CamelExceptionCaught as String ?: ''
+    def hasErrorDetailsTag      = inboundPayload =~ '<errordetails>' ? true : false
+    def exceptionMessages       = []
+
+    if (hasErrorDetailsTag) {
+        def xmlErrorPath        = new XmlSlurper().parseText(inboundPayload)
+        def xErrorDetailsNode   = xmlErrorPath.'**'.find { xmlTag -> xmlTag.name() == 'errordetails' }
+
+        xErrorDetailsNode.'**'.findAll { xmlTag -> xmlTag.name() == 'message' }.each { messageTag ->
+            exceptionMessages << messageTag.text()
+        }
+    }
+
+    exceptionMessages = exceptionMessages.isEmpty() ? camelExceptionCaught : exceptionMessages.join('\n')
+    return message
+}
