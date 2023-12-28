0e6e7ba44ddd29506a058d5493b1e7401683c91e
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2023-12-27 21:24:31 -0300

    refactor: dataStore operations

--- dataStore/read.groovy
+++ dataStore/read.groovy
@@ -1,13 +1,13 @@
 import com.sap.gateway.ip.core.customdev.util.Message
-import com.sap.it.api.asdk.datastore.*
-import com.sap.it.api.asdk.runtime.*
+import com.sap.it.api.asdk.datastore.DataStoreService
+import com.sap.it.api.asdk.runtime.Factory
 
-Message processData(Message message) {
-    def service     = new Factory(DataStoreService.class).getService()
-    if(service) {
-        def dsEntry = service.get("DatastoreName","EntryId")
+def processData(Message message) {
+    def service = Factory(DataStoreService).getService()
+    if (service) {
+        def dsEntry = service.get('DatastoreName', 'EntryId')
         def result  = new String(dsEntry.getDataAsArray())
-        message.setBody(body)
+        message.setBody(result)
     }
     return message
 }
