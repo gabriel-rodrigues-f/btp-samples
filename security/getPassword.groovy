144c7e0ffef339cd85db46689591a57e2fc7822c
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2023-12-27 21:59:11 -0300

    refactor: security operations

--- security/getPassword.groovy
+++ security/getPassword.groovy
@@ -1,21 +1,18 @@
-import com.sap.gateway.ip.core.customdev.util.Message;
-import java.util.HashMap;
-import com.sap.it.api.ITApiFactory;
-import com.sap.it.api.mapping.ValueMappingApi
+import com.sap.gateway.ip.core.customdev.util.Message
+import com.sap.it.api.ITApiFactory
 import com.sap.it.api.securestore.SecureStoreService
-import com.sap.it.api.securestore.UserCredential
 import com.sap.it.api.securestore.exception.SecureStoreException
 
 def Message processData(Message message) {
-    def apiKeyAlias             = message.getProperty("secureParameter")
+    def apiKeyAlias             = "APIKEYName"
     def secureStorageService    = ITApiFactory.getService(SecureStoreService.class, null)
-    
-    try{
+
+    try {
         def secureParameter     = secureStorageService.getUserCredential(apiKeyAlias)
         def apikey              = secureParameter.getPassword().toString()
-        message.setProperty("p_userpass", apikey)
-    } catch(Exception e){
-        throw new SecureStoreException("Secure Parameter not available")
+        message.setProperty('pUserPassword', apikey)
+    } catch (Exception e) {
+        throw new SecureStoreException('Secure Parameter not available')
     }
     return message
 }
