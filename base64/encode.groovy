d95c2f22761b3f000260b731c0028ca89d23e56c
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2023-12-27 21:20:00 -0300

    refactor: base64 parser

--- base64/encode.groovy
+++ base64/encode.groovy
@@ -3,6 +3,7 @@ import com.sap.gateway.ip.core.customdev.util.Message
 Message processData(Message message) {
     def payload         = "testando"
     def encodedPayload  = payload.bytes.encodeBase64().toString()
-    print encodedPayload
+    
+    message.setBody(encodedPayload)
     return message
 }
