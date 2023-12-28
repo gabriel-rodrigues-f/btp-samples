bcb893d2a789cd14ee90248ea0b6dc1933d941e4
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2023-12-27 21:21:22 -0300

    refactor: counters

--- counters/payloadSizeCounter.groovy
+++ counters/payloadSizeCounter.groovy
@@ -2,13 +2,12 @@ import com.sap.gateway.ip.core.customdev.util.Message
 import java.nio.charset.StandardCharsets
 
 def Message processData(Message message) {
-    
     def body        = message.getBody(String)
-    def pCounter    = message.getProperty("pCounter")
+    def pCounter    = message.getProperty('pCounter')
     def messageSize = body.getBytes().length
-        
+
     messageSize += pCounter.toInteger()
 
-    message.setProperty("pCounter", messageSize.toInteger())
+    message.setProperty('pCounter', messageSize.toInteger())
     return message
 }
