734569924a0760ced978213eed930a8ea6c7d9fa
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2023-12-27 21:33:51 -0300

    refactor: retry mechanism

--- exception/retryMechanism/retryMechanism.groovy
+++ exception/retryMechanism/retryMechanism.groovy
@@ -1,21 +1,21 @@
 import com.sap.gateway.ip.core.customdev.util.Message
 
-def Message processData(Message message){
-    def map             = message.getProperties()
-    String pRetorna     = map.get("pRetorna") as String
-	String pRetornoErro = map.get("CamelExceptionCaught") as String
-	def pResult         = "0"
+def Message processData(Message message) {
+    def properties      = message.getProperties()
+    def errorResponse   = properties.CamelExceptionCaught as String ?: ''
 
-	if (pRetornoErro.contains("Read timed out")
-		|| pRetornoErro.contains("Connection reset")
-		|| pRetornoErro.contains("HTTP Request failed with error")
-		|| pRetornoErro.contains("Service Unavailable")
-		|| pRetorna.contains("Sim")
-		|| pRetornoErro.contains("bloqueado por usuário")
-		|| pRetornoErro.contains("Ordem de Referencia já localizada para a cotacao")
-    ) {	
-		pResult = "1"
-		message.setProperty("p_RealizeRetry", pResult)	
-	}	
-	return message
+    if (shouldTriggerRetry(errorResponse)) {
+        message.setProperty('p_RealizeRetry', 1)
+    }
+    return message
+}
+
+def shouldTriggerRetry(String errorResponse) {
+    def retryKeywords = [
+        'Read timed out'
+        ,'Connection reset'
+        ,'HTTP Request failed with error'
+        ,'Service Unavailable'
+        ]
+    return retryKeywords.any { error -> errorResponse.contains(error) }
 }
